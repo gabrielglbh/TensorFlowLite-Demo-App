084e7555ca6ff93f61b83bd31cae52125ac50319
Gabriel García <gaabummm@gmail.com>
2019-11-24 15:56:34 +0100

    Added Models for GSON

--- app/src/main/java/com/example/android/catsvsdogs/models/CatModel.kt
+++ app/src/main/java/com/example/android/catsvsdogs/models/CatModel.kt
@@ -1,2 +1,28 @@
 package com.example.android.catsvsdogs.models
 
+import com.google.gson.annotations.SerializedName
+
+class CatModel(breeds: List<String>, height: String, id: String, url: String, width: String) {
+    @SerializedName("breeds")
+    var breeds: List<String>? = null
+
+    @SerializedName("height")
+    var height: String? = null
+
+    @SerializedName("id")
+    var id: String? = null
+
+    @SerializedName("url")
+    var url: String? = null
+
+    @SerializedName("width")
+    var width: String? = null
+
+    init {
+        this.breeds = breeds
+        this.height = height
+        this.id = id
+        this.url = url
+        this.width = width
+    }
+}
--- app/src/main/java/com/example/android/catsvsdogs/models/DogModel.kt
+++ app/src/main/java/com/example/android/catsvsdogs/models/DogModel.kt
@@ -1,2 +1,18 @@
 package com.example.android.catsvsdogs.models
 
+import com.google.gson.annotations.SerializedName
+
+class DogModel(message: String, status: String) {
+
+    @SerializedName("message")
+    var message: String? = null
+
+    @SerializedName("status")
+    var status: String? = null
+
+    init {
+        this.message = message
+        this.status = status
+    }
+}
+
